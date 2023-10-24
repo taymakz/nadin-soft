ea17a81c3823866f79461a5b587a240e642eda53
Taymaz <taymazak1382@gmail.com>
2023-10-24 18:41:00 +0330

    side bar Links and I18n Setup

---
+++ components/AppAside.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <aside id="logo-sidebar" class="fixed left-0 top-0 z-40 h-screen w-64 border-r border-gray-200 bg-white pt-20 transition-transform -translate-x-full sm:translate-x-0 dark:border-gray-700 dark:bg-gray-800" aria-label="Sidebar">
+    <div class="h-full overflow-y-auto bg-white px-3 pb-4 dark:bg-gray-800">
+      <ul class="font-medium space-y-2">
+        <li>
+          <NuxtLink to="/" class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+            <i class="i-carbon-apps h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+            <span class="mr-3">{{ $t('dashboard') }}</span>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/todos" class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+            <i class="i-carbon-ibm-watson-knowledge-catalog h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+            <span class="mr-3">{{ $t('todos') }}</span>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/weather" class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+            <i class="i-carbon-radar-weather h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+            <span class="mr-3">{{ $t('weather') }}</span>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/profile" class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+            <i class="i-carbon-customer-service h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+            <span class="mr-3">{{ $t('profile') }}</span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<style>
+
+</style>
--- i18n.config.ts
+++ i18n.config.ts
@@ -4,9 +4,17 @@ export default defineI18nConfig(() => ({
   messages: {
     en: {
       welcome: 'Welcome',
+      dashboard: 'Dashboard',
+      todos: 'Todos',
+      weather: 'Weather',
+      profile: 'Profile',
     },
     fa: {
       welcome: 'خوش آمدید',
+      dashboard: 'داشبورد',
+      todos: 'کار ها',
+      weather: 'وضعیت آب و هوا',
+      profile: 'پروفایل',
     },
   },
 }))
