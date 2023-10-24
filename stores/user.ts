5d4e177cce00659f10fd5a5f33166d11d7d86014
Taymaz <taymazak1382@gmail.com>
2023-10-24 21:43:20 +0330

    fixed bug

--- stores/user.ts
+++ stores/user.ts
@@ -5,17 +5,15 @@ export const useUserStore = defineStore('user', () => {
   const user: Ref<string | null> = ref(null)
   const isLogin = computed(() => user.value != null)
   const setName = async (value: string) => {
-    const storage = useStorage()
     user.value = value
-    await storage.setItem('username', user.value)
+    await localStorage.setItem('username', user.value)
   }
   const init = async () => {
-    const storage = useStorage()
-    const username = await storage.getItem('username')
-    if (username)
+    const username = await localStorage.getItem('username')
+    if (username !== null)
       user.value = username.toString()
     else
-      navigateTo('/')
+      await navigateTo('/')
   }
 
   return { user, setName, isLogin, init }
