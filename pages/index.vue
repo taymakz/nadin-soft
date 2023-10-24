807c6bd3a5c620d9652dc526b8574ad8c5db2cc8
Taymaz <taymazak1382@gmail.com>
2023-10-24 21:58:56 +0330

    index page Done

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,55 @@
 <script setup lang="ts">
-const { locale } = useI18n()
+import { useField } from 'vee-validate'
 
-function toggleLocate() {
-  locale.value = locale.value === 'en' ? 'fa' : 'en'
+const { t, locale, setLocale } = useI18n()
+const userStore = useUserStore()
+
+definePageMeta({
+  layout: false,
+})
+
+const isRequired = (val: string) => (val.trim() ? true : t('required_input'))
+
+const { errorMessage: usernameError, value: name } = useField('username', isRequired)
+
+async function submit() {
+  if (!name.value)
+    return
+  await userStore.setName(name.value)
+  await navigateTo('/dashboard')
 }
+function toggleLocale() {
+  setLocale(locale.value === 'en' ? 'fa' : 'en')
+}
+
+onMounted(async() => {
+  const userStore = await useUserStore()
+  await userStore.init()
+  if (userStore.isLogin)
+    navigateTo('/dashboard')
+})
 </script>
 
 <template>
-  <div>
-    <!-- data-aos="fade-up" -->
-    <div class="container">
-      <div class="flex flex-col justify-center gap-y-4">
-       
-        <button class="btn" @click="toggleLocate">
-          Change Language
-        </button>
-      </div>
+  <div class="min-h-screen flex items-center justify-center bg-white dark:bg-zinc-900">
+    <div class="flex flex-col gap-y-4">
+      <AInput
+        v-model="name"
+        :error="usernameError"
+        :label="$t('name')"
+        type="text"
+      />
+
+      <ABtn color="success" @click="submit">
+        {{ $t('save') }}
+      </ABtn>
+
+      <ABtn color="success" @click="toggleLocale">
+        <span v-if="locale === 'en'">Language : {{ $t('persian') }} </span>
+        <span v-else>
+          زبان : {{ $t('english') }}
+        </span>
+      </ABtn>
     </div>
   </div>
 </template>
