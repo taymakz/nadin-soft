f8358693d835997d829daeb601b533ebadb788c3
Taymaz <taymazak1382@gmail.com>
2023-10-24 21:44:16 +0330

    removed unUsed ref

--- stores/user.ts
+++ stores/user.ts
@@ -3,7 +3,6 @@ import type { Ref } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
   const user: Ref<string | null> = ref(null)
-  const isLogin = computed(() => user.value != null)
   const setName = async (value: string) => {
     user.value = value
     await localStorage.setItem('username', user.value)
@@ -16,5 +15,5 @@ export const useUserStore = defineStore('user', () => {
       await navigateTo('/')
   }
 
-  return { user, setName, isLogin, init }
+  return { user, setName, init }
 })
