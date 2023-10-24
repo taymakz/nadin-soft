54d0dc902a04f178e2cdd7e26d8e0800b5f43e3d
Taymaz <taymazak1382@gmail.com>
2023-10-24 21:33:18 +0330

    init user store in app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { appName } from '~/constants'
 
+const userStore = useUserStore()
+onMounted(async () => {
+  await userStore.init()
+})
 useHead({
   title: appName,
-  htmlAttrs:{
-    dir:'rtl',
-    lang:'fa'
-  }
+  htmlAttrs: {
+    dir: 'rtl',
+    lang: 'fa',
+  },
 })
 </script>
 
