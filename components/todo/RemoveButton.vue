14569e610aab06175fb01526ae0d9abfbee0383f
TaymaZ <taymazak1382@gmail.com>
2023-11-22 13:26:00 +0330

    feat: added RemoveButton Todo Component

---
+++ components/todo/RemoveButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <ABtn icon="i-carbon-trash-can" icon-only variant="text" @click="removeTodo" />
+</template>
+
+<script lang="ts" setup>
+const userStore = useUserStore()
+const props = defineProps({
+  itemId: {
+    type: Number,
+    required: true
+  }
+})
+const removeTodo=()=>{
+  userStore.removeTodo(props.itemId)
+}
+</script>
